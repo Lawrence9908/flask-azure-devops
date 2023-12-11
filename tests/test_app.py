82d39523974adbd4e1f16872fc7e292816a3dee1
Lawrence9908 <mugwenalawrence121@gmail.com>
2023-12-11 20:22:30 +0000

    add makefile

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+# tests/test_app.py
+
+from app import app
+import pytest
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+
+def test_hello(client):
+    response = client.get('/')
+    assert b'Hello, Azure DevOps!' in response.data
